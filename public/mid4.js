adb191aacfeafac61fd6093ac68c378cb3af00a1
HongJin-Jeong <mistake9518@gmail.com>
2020-05-06 11:06:03 +0900

    중간고사 최종본

--- public/mid4.js
+++ public/mid4.js
@@ -1,16 +1,13 @@
 let a = [];
-
+let cnt =0;
 for(let i=0; i<100;++i){
     a[i]=Math.floor(Math.random()*100 +1);
 }
 
-function callback(a, b) { 
-    if(!a[b]){
-        a[b] =0;
-    }
+function callback(a, b) {
     if(a%2==0 & b%2==0){
-        a[b]++;
-        console.log(a[b]);
+        cnt++;
+        console.log(cnt);
             return a + b; 
         }else{
             return a + b; 
